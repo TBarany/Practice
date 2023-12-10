55b15f61c016870ec48667c877bc2904ebf76fc4
TamasBarany <tamas.barany97@gmail.com>
2023-12-10 18:06:13 +0100

    node added

---
+++ app.js
@@ -0,0 +1,36 @@
+// jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+mongoose.connect("mongodb+srv://admin:admin@cluster0.hvqqeen.mongodb.net/geekShopDB?retryWrites=true&w=majority");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+let Movie = mongoose.model('Movie', 
+               new Schema({ title: String}), 
+               'movies');   
+
+
+
+
+
+// Works
+
+
+app.get("/",async (req,res)=>{
+  let result = await Movie.find();
+
+  res.send(`${result}`)
+})
+
+app.listen(process.env.PORT, '0.0.0.0', function(err) {
+  console.log("Started listening on %s", app.url);
+});
+
+
+
+
