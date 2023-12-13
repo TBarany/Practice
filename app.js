384020e3aaf10b420e2bb185804bf8593e746961
TamasBarany <tamas.barany97@gmail.com>
2023-12-13 21:26:12 +0100

    changed

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ let Movie = mongoose.model('Movie',
 
 
 app.get("/",async (req,res)=>{
- /* let result = await Movie.find(); */
- let result = {name: "Feri"}
+ let result = await Movie.find();
 
 
   res.send(JSON.stringify(result))
